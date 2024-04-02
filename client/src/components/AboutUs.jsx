324ba82e89a7dfe2185d618ea1717c055fd44982
Kishan <kissanshaa@gmail.com>
2024-04-02 21:29:14 +0530

    project done

---
+++ client/src/components/AboutUs.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import image from "../images/aboutimg.jpeg";
+
+const AboutUs = () => {
+  return (
+    <>
+      <section className="container">
+        <h2 className="page-heading about-heading">About Us</h2>
+        <div className="about">
+          <div className="hero-img">
+            <img src={image} alt="hero" />
+          </div>
+          <div className="hero-content">
+            {/* Add a div to start the paragraph from image line */}
+            <div>
+              <p>
+                MediConnect, a preeminent healthcare platform dedicated to
+                empowering individuals globally, stands firm in its commitment
+                to harnessing innovative technology and delivering personalized
+                care to offer accessible and comprehensive healthcare solutions.
+                At the heart of our endeavor lies a profound mission: to
+                spearhead a paradigm shift in healthcare, thereby guaranteeing
+                universal access to quality services and unwavering support for
+                all.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default AboutUs;
---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import image from "../images/heroimg.jpg";
+import "../styles/hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>
+          Your Health, <br />
+          Our Responsibility
+        </h1>
+        <p>
+          At MediConnect, we take pride in making your health our utmost
+          responsibility. Our dedicated team is committed to providing
+          personalized care and support tailored to your individual needs. With
+          advanced technology and a compassionate approach, we strive to empower
+          you on your journey to better health and well-being. Trust us to
+          prioritize your health and ensure you receive the highest quality of
+          care every step of the way.
+        </p>
+      </div>
+      <div className="hero-img">
+        <img src={image} alt="hero" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
